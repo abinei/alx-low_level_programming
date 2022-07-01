0e85596b90e5b61bc26b87aa5a65da5f7e09e226
abinei <abineabi@gmail.com>
2022-06-30 17:57:54 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,9 @@
 #include<stdio.h>
-int main()
+int main(void)
 {
 
-	puts("Programming is like building a multilingual puzzle");
-	
-	return 0;
+	puts("Programming is like building a multilingual puzze.\n");
+	return (0);
 }
 
 
